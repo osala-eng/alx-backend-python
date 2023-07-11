727540dcda140906f600b116337514d1a490f3b6
Jashon Osala <jashonosala@gmail.com>
2023-07-11 05:29:52 +0300

    Restructure concurrent routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,30 +2,11 @@
 """
 Multiple coroutines at the same time with async
 """
-
 import asyncio
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int = 0, max_delay: int = 10) -> list:
-    """[summary]
-
-    Args:
-        n (int, optional): [description]. Defaults to 0.
-        max_delay (int, optional): [description]. Defaults to 10.
-
-    Returns:
-        list: [description] - list of all the delays (float values)
-        in assending order
-    """
+    """list: [description] - list of all the delays (float values)"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delay = [await task for task in asyncio.as_completed(tasks)]
-    return delay
-
-
-if __name__ == "__main__":
-    print(asyncio.run(wait_n(5, 5)))
-    print(asyncio.run(wait_n(10, 7)))
-    print(asyncio.run(wait_n(15, 0)))
+    return [await task for task in asyncio.as_completed(tasks)]
