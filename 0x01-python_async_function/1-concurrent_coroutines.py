0fb43f034f2caf226aede3c7abddd09886536a0d
Jashon Osala <jashonosala@gmail.com>
2023-07-11 03:31:21 +0300

    Concurrent routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,7 +21,8 @@ async def wait_n(n: int = 0, max_delay: int = 10) -> list:
         in assending order
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    delay = [await task for task in asyncio.as_completed(tasks)]
+    return delay
 
 
 if __name__ == "__main__":
