e370b04039cbfa154f23ee80d52efaf9a34219fb
Jashon Osala <jashonosala@gmail.com>
2023-08-01 04:03:58 +0300

    Test public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,3 +32,18 @@ class TestGithubOrgClient(unittest.TestCase):
                    PropertyMock(return_value=result)):
             response = GithubOrgClient(name)._public_repos_url
             self.assertEqual(response, result.get('repos_url'))
+
+    @parameterized.expand([
+        ("random_url", {"public_repo_url": "https://some.com"})
+    ])
+    @patch('client.get_json')
+    def test_public_repos(self, name, result, mock_json):
+        """
+        Test that the list of repos is what you expect
+        from the chosen payload.
+        """
+        with patch('GithubOrgClient._public_repos_url',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name).public_repos()
+            self.assertEqual(response, result.get('public_repo_url'))
+            mock_json.assert_called_once()
